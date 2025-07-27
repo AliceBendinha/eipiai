e02c0a7da7fadf539a5a4ffdb8362f216a208292
Alice D'Costa <bendinhacalice@gmail.com>
2025-07-27 22:12:04 +0100

    Alterado Nomes de Propriedade para CamelCase

--- eipiai/ViewModel/LivrosViewModel.cs
+++ eipiai/ViewModel/LivrosViewModel.cs
@@ -3,13 +3,13 @@
     public class LivrosViewModel
     {
         
-        public string titulo { get; private set; }
+        public string Titulo { get; private set; }
         
-        public string autor { get; private set; }
+        public string Autor { get; private set; }
         
-        public string genero { get; private set; }
+        public string Genero { get; private set; }
         
-        public string ano_lancamento { get; private set; }
+        public string Ano_Lancamento { get; private set; }
 
     }
 }
